538f05598efb49c5366e3247b05a254c5f83a596
bdnkai <bdn.kai@gmail.com>
2022-08-30 18:53:10 -0700

    lab for redux (old)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import allReducer from './reducers/index'
 
+const store = configureStore(allReducer)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
